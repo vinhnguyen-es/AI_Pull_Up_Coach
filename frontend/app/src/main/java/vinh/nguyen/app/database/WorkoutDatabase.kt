d16c6c67199acbe7e8ed831a89af65e23268a632
JamieLandolt <jamielandolt@outlook.com>
2026-01-19 16:08:43 +0700

    Length fixed and displays in MM:SS format in  the DB as a String

--- frontend/app/src/main/java/vinh/nguyen/app/database/WorkoutDatabase.kt
+++ frontend/app/src/main/java/vinh/nguyen/app/database/WorkoutDatabase.kt
@@ -21,7 +21,7 @@ abstract class WorkoutDatabase : RoomDatabase() {
         val MIGRATION_2_3 = object : Migration(2, 3) {
             override fun migrate(database: SupportSQLiteDatabase) {
                 // Example: Add a new integer column 'age' with a default value of 0
-                database.execSQL("ALTER TABLE workouts ADD COLUMN date CHAR255 NOT NULL DEFAULT ''")
+                database.execSQL("DELETE FROM workouts;\nALTER TABLE workouts ALTER COLUMN date CHAR255 NOT NULL;\n")
             }
         }
 
@@ -30,7 +30,7 @@ abstract class WorkoutDatabase : RoomDatabase() {
             // if the Instance is not null, return it, otherwise create a new database instance.
             return Instance ?: synchronized(this) {
                 Room.databaseBuilder(context, WorkoutDatabase::class.java, "workout_database")
-//                    .addMigrations(MIGRATION_1_2)
+                    .addMigrations(MIGRATION_2_3)
                     .build()
                     .also { Instance = it }
             }
